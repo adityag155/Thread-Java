cee79e848dbe1105f3a65b9cf902d874f1def45e
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-19 21:44:28 +0530

    Add files via upload

--- ThreadCode.java
+++ ThreadCode.java
@@ -1,10 +1,17 @@
-class ThreadLogic1 implements Runnable
+class MyThread implements Runnable
 {
+Thread t;
+MyThread(String tname)
+{
+t=new Thread(this, tname);
+t.start();
+}
+
 public void run()
 {
-for(int i=1; i<=5; i++)
+for(int i=1;i<=4;i++)
 {
-System.out.println(Thread.currentThread().getName()+":"+i);
+System.out.println(t.getName()+":"+i);
 try
 {
 Thread.sleep(1000);
@@ -14,56 +21,52 @@ catch(InterruptedException ie)
 }
 }
 }
-
-class ThreadLogic2 implements Runnable
-{
-public void run()
-{
-for(int i=6; i<=10; i++)
-{
-System.out.println(Thread.currentThread().getName()+":"+i);
-try
-{
-Thread.sleep(1000);
-}
-catch(InterruptedException ie)
-{}
-}
-}
-}
-
-class MyThread
-{
-Thread t1,t2;
-MyThread(String tname1, String tname2)
-{
-t1=new Thread(new ThreadLogic1(),tname1);
-t2=new Thread(new ThreadLogic2(),tname2);
-
-t1.start();
-t2.start();
-}
-}
-
 
 class ThreadCode
 {
 public static void main(String args[])
 {
-new MyThread("One","Two");
+MyThread m1=new MyThread("One");
+MyThread m2=new MyThread("Two");
+MyThread m3=new MyThread("Three");
+
+System.out.println(m1.t.isAlive());
+System.out.println(m2.t.isAlive());
+System.out.println(m3.t.isAlive());
+try
+{
+m1.t.join();
+m2.t.join();
+m3.t.join();
+}
+catch(InterruptedException ie)
+{}
+
+
+System.out.println(m1.t.isAlive());
+System.out.println(m2.t.isAlive());
+System.out.println(m3.t.isAlive());
 }
 }
 
 /*
 O/P:
+true
+true
+true
+Three:1
 One:1
-Two:6
+Two:1
+Three:2
 One:2
-Two:7
+Two:2
+Three:3
 One:3
-Two:8
+Two:3
+Three:4
 One:4
-Two:9
-One:5
-Two:10
+Two:4
+false
+false
+false
 */
