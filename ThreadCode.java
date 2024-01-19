2ddf3c9f520fab63684f4a042ba9300e03c36b2d
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-19 21:35:47 +0530

    Add files via upload

--- ThreadCode.java
+++ ThreadCode.java
@@ -1,17 +1,10 @@
-class MyThread implements Runnable
+class ThreadLogic1 implements Runnable
 {
-Thread t;
-MyThread(String tname)
-{
-t=new Thread(this, tname);
-t.start();
-}
-
 public void run()
 {
-for(int i=1;i<=4;i++)
+for(int i=1; i<=5; i++)
 {
-System.out.println(t.getName()+":"+i);
+System.out.println(Thread.currentThread().getName()+":"+i);
 try
 {
 Thread.sleep(1000);
@@ -22,28 +15,55 @@ catch(InterruptedException ie)
 }
 }
 
+class ThreadLogic2 implements Runnable
+{
+public void run()
+{
+for(int i=6; i<=10; i++)
+{
+System.out.println(Thread.currentThread().getName()+":"+i);
+try
+{
+Thread.sleep(1000);
+}
+catch(InterruptedException ie)
+{}
+}
+}
+}
+
+class MyThread
+{
+Thread t1,t2;
+MyThread(String tname1, String tname2)
+{
+t1=new Thread(new ThreadLogic1(),tname1);
+t2=new Thread(new ThreadLogic2(),tname2);
+
+t1.start();
+t2.start();
+}
+}
+
+
 class ThreadCode
 {
 public static void main(String args[])
 {
-new MyThread("One");
-new MyThread("Two");
-new MyThread("Three");
+new MyThread("One","Two");
 }
 }
 
 /*
 O/P:
-Two:1
-Three:1
 One:1
-Two:2
+Two:6
 One:2
-Three:2
-Two:3
+Two:7
 One:3
-Three:3
-Two:4
+Two:8
 One:4
-Three:4
+Two:9
+One:5
+Two:10
 */
