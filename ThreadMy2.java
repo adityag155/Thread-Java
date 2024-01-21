4d10c043ae36a9df7f087c9fbd4c797cb5e904d7
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-21 23:33:42 +0530

    Add files via upload

---
+++ ThreadMy2.java
@@ -0,0 +1,54 @@
+class ThreadLogic1 implements Runnable
+{
+public void run()
+{
+for(int i=1;i<=10;i++)
+{
+System.out.println(Thread.currentThread().getName()+":"+i);
+try
+{
+Thread.sleep(1000);
+}
+catch(InterruptedException ie)
+{}
+}
+}
+}
+
+class ThreadLogic2 implements Runnable
+{
+public void run()
+{
+for(int i=11;i<=20;i++)
+{
+System.out.println(Thread.currentThread().getName()+":"+i);
+try
+{
+Thread.sleep(1000);
+}
+catch(InterruptedException ie)
+{}
+}
+}
+}
+
+class MyThread
+{
+Thread t1,t2;
+MyThread(String tname1,String tname2)
+{
+t1=new Thread(new ThreadLogic1(),tname1);
+t2=new Thread(new ThreadLogic2(),tname2);
+
+t1.start();
+t2.start();
+}
+}
+
+class ThreadMy2
+{
+public static void main(String args[])
+{
+new MyThread("One","Two");
+}
+}
